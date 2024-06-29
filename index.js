f95a31e42b83be0b0ca34209d9a940bce968bb71
Xydilz <danangoneul@gmail.com>
2024-06-29 13:55:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ const menu = require("./lib/menu.js")
 
 const apivhtear = 'Apikey vhtear';
 const apibarbar = 'Apikey mhankbarbar';
-const BotName = 'VVIPINDO BOT'; 
-const instagram = 'agus_alnsyh71- kaelMk_'; 
+const BotName = 'DILZBOTT'; 
+const instagram = 'xydilzkcebet'; 
 const aktif = 'Kapan bot aktif';
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:Agus\n' // Nama kamu
-            + 'ORG:VVIPINDO;\n' // Nama bot
-            + 'TEL;type=CELL;type=VOICE;waid=6289613469459:+62 896-1346-9459\n' //Nomor whatsapp kamu
+            + 'FN:Fadilz\n' // Nama kamu
+            + 'ORG:DILZBOTT;\n' // Nama bot
+            + 'TEL;type=CELL;type=VOICE;waid=6287865150554:+62 878-6515-0554\n' //Nomor whatsapp kamu
             + 'END:VCARD'
 const
 {
